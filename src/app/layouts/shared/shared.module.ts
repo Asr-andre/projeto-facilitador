d170b2e36f11ec9b754626ff621638926e43b91c
Asr-andre <suport.info.log@gmail.com>
2024-07-09 14:04:11 -0300

    Retirada de components desnecesarios e imagens que estavam sendo caregando com a aplicação

--- src/app/layouts/shared/shared.module.ts
+++ src/app/layouts/shared/shared.module.ts
@@ -14,13 +14,12 @@ import { TranslateModule } from '@ngx-translate/core';
 import { TopbarComponent } from './topbar/topbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { RightsidebarComponent } from './rightsidebar/rightsidebar.component';
 import { HorizontaltopbarComponent } from './horizontaltopbar/horizontaltopbar.component';
 import { HorizontalnavbarComponent } from './horizontalnavbar/horizontalnavbar.component';
 
 @NgModule({
   // tslint:disable-next-line: max-line-length
-  declarations: [TopbarComponent, FooterComponent, SidebarComponent, RightsidebarComponent, HorizontaltopbarComponent, HorizontalnavbarComponent],
+  declarations: [TopbarComponent, FooterComponent, SidebarComponent, HorizontaltopbarComponent, HorizontalnavbarComponent],
   imports: [
     CommonModule,
     TranslateModule,
@@ -31,7 +30,7 @@ import { HorizontalnavbarComponent } from './horizontalnavbar/horizontalnavbar.c
     FormsModule,
     ReactiveFormsModule
   ],
-  exports: [TopbarComponent, FooterComponent, SidebarComponent, RightsidebarComponent, HorizontaltopbarComponent, HorizontalnavbarComponent],
+  exports: [TopbarComponent, FooterComponent, SidebarComponent, HorizontaltopbarComponent, HorizontalnavbarComponent],
   providers: [LanguageService]
 })
 export class SharedModule { }
