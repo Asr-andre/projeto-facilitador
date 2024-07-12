d3fb3d6f9a5b718ccaccfd2ea68a13978d224aa7
Asr-andre <suport.info.log@gmail.com>
2024-07-12 11:52:44 -0300

    Inclusão dos campos do modal de cadastro do contratante

--- src/app/pages/cadastro/contratantes/contratantes.component.ts
+++ src/app/pages/cadastro/contratantes/contratantes.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Utils } from 'src/app/core/helpers/utils';
 import { ContratanteModel } from 'src/app/core/models/cadastro/contratante.model';
 import { AlertService } from 'src/app/core/services/alert.service';
+import { AuthenticationService } from 'src/app/core/services/auth.service';
 import { ContratanteService } from 'src/app/core/services/cadastro/contratante.service';
 
 @Component({
@@ -12,16 +14,36 @@ import { ContratanteService } from 'src/app/core/services/cadastro/contratante.s
 })
 export class ContratantesComponent implements OnInit {
   public contratantes: ContratanteModel[] = [];
+  public formContratante: FormGroup;
   public loading: boolean;
 
   constructor(
     private _contratanteService: ContratanteService,
+    private _formBuilder: FormBuilder,
     private _modalService: NgbModal,
+    private _authenticationService: AuthenticationService,
     private _alertService: AlertService
   ) { }
 
   ngOnInit(): void {
     this.obterContratantes();
+    this.inicializarformContratante();
+  }
+
+  public inicializarformContratante() {
+    this.formContratante = this._formBuilder.group({
+      id_empresa: [''],
+      cnpj: ["", Validators.required],
+      razao_social: ["", Validators.required],
+      fantasia: ["", Validators.required],
+      endereco: [""],
+      numero: [""],
+      complemento: [""],
+      bairro: [""],
+      cidade: [""],
+      uf: [""],
+      user_login: [this._authenticationService.getLogin()],
+    });
   }
 
   public obterContratantes() {
