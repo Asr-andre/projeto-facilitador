3f89357cf6d703e0cb987415f3f08df0544dfd2b
Asr-andre <suport.info.log@gmail.com>
2024-07-26 18:33:11 -0300

    Retirada de modulos desnecessarios

--- src/app/pages/processamentos/processamentos.module.ts
+++ src/app/pages/processamentos/processamentos.module.ts
@@ -2,19 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-
-import { CdkStepperModule } from '@angular/cdk/stepper';
-import { NgStepperModule } from 'angular-ng-stepper';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { UiSwitchModule } from 'ngx-ui-switch';
-import { ColorPickerModule } from 'ngx-color-picker';
-
-import { UiModule } from '../../shared/ui/ui.module';
-
 import { ImportacaoRoutingModule } from './processamentos-routing.module';
 import { ImportacaoComponent } from './importacao/importacao.component';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -28,16 +17,8 @@ import { ImportacaoManualComponent } from './importacao/componente/importacao-ma
     FormsModule,
     ReactiveFormsModule,
     ImportacaoRoutingModule,
-    UiModule,
-    CKEditorModule,
-    CdkStepperModule,
-    NgStepperModule,
     NgxMaskDirective,
     NgxMaskPipe,
-    NgSelectModule,
-    UiSwitchModule,
-    ColorPickerModule,
-    NgbDatepickerModule,
     NgxPaginationModule,
     NgbModule
   ],
