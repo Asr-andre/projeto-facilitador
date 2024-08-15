cfaccd3ec8ddee581251e269247634057eef18d1
andre <suport.info.log@gmail.com>
2024-08-15 17:54:23 -0300

    retirado o ussl

--- src/app/core/services/url.base.service.ts
+++ src/app/core/services/url.base.service.ts
@@ -1,5 +1,5 @@
 
 export const AppConfig = {
   //apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://portal.facilitadorsistemas.com.br:3000'
+  apiUrl: 'http://portal.facilitadorsistemas.com.br:3000'
 };
