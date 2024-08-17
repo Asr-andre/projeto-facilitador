67455882796f5d4727b63bd5a1eee934675ecf41
andre <suport.info.log@gmail.com>
2024-08-17 20:07:58 -0300

    Criação da tela de relatorio dos pagamentos

--- src/app/pages/relatorios/financeiro/financeiro.component.ts
+++ src/app/pages/relatorios/financeiro/financeiro.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './financeiro.component.scss'
 })
 export class FinanceiroComponent {
+  public contratanteSelecionado: number;
+  public filtros: boolean = false;
+  public loading: boolean =false;
+  public ativaAba: number = 1;
+  public selecionarTodos: boolean = false;
+  public idUsuario: number | null = null;
+
+  public textoPesquisa: string = "";
+  public totalRegistros: number = 0;
+
+  public totalClientes: number = 0;
+  public valorTotal: number = 0;
+
+  totalClientesSelecionados: number = 0;
+  valorTotalSelecionado: number = 0;
+
+  constructor(
+
+  ) { }
 
 }
--- src/app/pages/relatorios/relatorios.module.ts
+++ src/app/pages/relatorios/relatorios.module.ts
@@ -1,57 +1,26 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-
-import { CdkStepperModule } from '@angular/cdk/stepper';
-import { NgStepperModule } from 'angular-ng-stepper';
-
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { UiSwitchModule } from 'ngx-ui-switch';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { DropzoneModule } from 'ngx-dropzone-wrapper';
-
-import { UiModule } from '../../shared/ui/ui.module';
-import { DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
-import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
+import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RelatoriosRoutingModule } from './relatorios.routing.module';
+import { FinanceiroComponent } from './financeiro/financeiro.component';
 
 
-const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
-  // Change this to your upload POST address:
-  url: 'https://httpbin.org/post',
-  maxFilesize: 50
-};
 @NgModule({
 
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, FinanceiroComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     RelatoriosRoutingModule,
-    UiModule,
-    CKEditorModule,
-    CdkStepperModule,
-    NgStepperModule,
-    NgxMaskDirective,
-    NgxMaskPipe,
-    NgSelectModule,
-    UiSwitchModule,
-    ColorPickerModule,
     NgbDatepickerModule,
-    DropzoneModule
+    NgbModule,
+    NgbModule
   ],
   providers: [
-    provideNgxMask(),
-    {
-      provide: DROPZONE_CONFIG,
-      useValue: DEFAULT_DROPZONE_CONFIG
-    }
+
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
