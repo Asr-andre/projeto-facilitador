90b2cc752e00987508fe66c54f5f81593474e23d
andre <suport.info.log@gmail.com>
2024-09-03 22:34:34 -0300

    Retirada de componentes desnecessarios

--- src/app/extrapages/extrapages-routing.module.ts
+++ src/app/extrapages/extrapages-routing.module.ts
@@ -1,24 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { MaintenanceComponent } from './maintenance/maintenance.component';
 import { Page404Component } from './page404/page404.component';
 import { Page500Component } from './page500/page500.component';
-import { Login1Component } from './login1/login1.component';
-import { Register1Component } from './register1/register1.component';
-import { RecoverpasswordComponent } from './recoverpassword/recoverpassword.component';
-import { LockscreenComponent } from './lockscreen/lockscreen.component';
-import { ComingsoonComponent } from './comingsoon/comingsoon.component';
 
 const routes: Routes = [
-    {
-        path: 'maintenance',
-        component: MaintenanceComponent
-    },
-    {
-        path: 'coming-soon',
-        component: ComingsoonComponent
-    },
     {
         path: '404',
         component: Page404Component
@@ -26,22 +11,6 @@ const routes: Routes = [
     {
         path: '500',
         component: Page500Component
-    },
-    {
-        path: 'login-1',
-        component: Login1Component
-    },
-    {
-        path: 'register-1',
-        component: Register1Component
-    },
-    {
-        path: 'recoverpwd-1',
-        component: RecoverpasswordComponent
-    },
-    {
-        path: 'lock-screen-1',
-        component: LockscreenComponent
     }
 ];
 
