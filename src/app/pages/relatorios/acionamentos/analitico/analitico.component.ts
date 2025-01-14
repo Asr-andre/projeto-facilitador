5f91605f22b5346d51bae0cf3d2ebba178ce9882
Asr-andre <suport.info.log@gmail.com>
2025-01-14 11:04:42 -0300

    Retirado condigos desnecessarios

--- src/app/pages/relatorios/acionamentos/analitico/analitico.component.ts
+++ src/app/pages/relatorios/acionamentos/analitico/analitico.component.ts
@@ -1,9 +1,8 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Utils } from 'src/app/core/helpers/utils';
 import { AcionamentoAnaliticoModel, RequisicaoAcionamentoModel } from 'src/app/core/models/relatorio/acionamento.model';
-import { AlertService } from 'src/app/core/services/alert.service';
-import { AuthenticationService } from 'src/app/core/services/auth.service';
+import { AlertService } from 'src/app/core/services/alert.service';;
 import { ExcelService } from 'src/app/core/services/excel.service';
 import { AcionamentoService } from 'src/app/core/services/relatorio/acionamento.service';
 
@@ -12,33 +11,26 @@ import { AcionamentoService } from 'src/app/core/services/relatorio/acionamento.
   templateUrl: './analitico.component.html',
   styleUrl: './analitico.component.scss'
 })
-export class AnaliticoComponent implements OnChanges, OnInit {
+
+export class AnaliticoComponent implements OnChanges {
   @Input() filtros: RequisicaoAcionamentoModel;
-  public idEmpresa: number = Number(this._auth.getIdEmpresa() || 0);
-  public idUsuario: number = Number(this._auth.getCurrentUser() || 0);
-  public login = this._auth.getLogin();
   public loadingMin: boolean = false;
   public acio_Analitico: AcionamentoAnaliticoModel[] = [];
 
   constructor(
     private _acionamentoService: AcionamentoService,
-    private _auth: AuthenticationService,
     private _alert: AlertService,
     private _datePipe: DatePipe,
     private _excelService: ExcelService,
   ) { }
 
-  ngOnInit() {
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['filtros'] && this.filtros) {
-      this.obterAcionamentosAnalitico();
+      this.acionamentosAnalitico();
     }
   }
 
-  public obterAcionamentosAnalitico() {
-    console.log(this.filtros)
+  public acionamentosAnalitico() {
     const dadosParaEnvio = { ...this.filtros };
     dadosParaEnvio.data_inicio = this._datePipe.transform(dadosParaEnvio.data_inicio, "dd/MM/yyyy");
     dadosParaEnvio.data_fim = this._datePipe.transform(dadosParaEnvio.data_fim, "dd/MM/yyyy");
@@ -50,7 +42,6 @@ export class AnaliticoComponent implements OnChanges, OnInit {
         if (res.success === 'true') {
           this.acio_Analitico = res.dados;
 
-          // Ordena a coluna data em ordem descendente
           this.acio_Analitico.sort((a, b) => {
             if (a.data_acio < b.data_acio) return 1;
             if (a.data_acio > b.data_acio) return -1;
