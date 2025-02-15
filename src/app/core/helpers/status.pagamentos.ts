44abc0e922682f927e59834daceef4cd50e7d43b
andre <suport.info.log@gmail.com>
2025-02-14 21:58:50 -0300

    Criação de constantes para usar futuramente no sistema

---
+++ src/app/core/helpers/status.pagamentos.ts
@@ -0,0 +1,6 @@
+export const STATUS_PAGAMENTO = [
+  { value: 'pendente', label: 'Pendente' },
+  { value: 'aprovado', label: 'Aprovado' },
+  { value: 'cancelado', label: 'Cancelado' },
+  { value: 'reembolsado', label: 'Reembolsado' }
+];
---
+++ src/app/core/helpers/tipo.usuario.ts
@@ -0,0 +1,6 @@
+export const TIPOS_USUARIO = [
+  { value: 'admin', label: 'Administrador' },
+  { value: 'gestor', label: 'Gestor' },
+  { value: 'cliente', label: 'Cliente' },
+  { value: 'suporte', label: 'Suporte' }
+];
