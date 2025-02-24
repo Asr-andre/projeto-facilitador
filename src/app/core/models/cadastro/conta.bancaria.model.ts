3ef0128199c61dcd876dae976085b0319900005a
andre <suport.info.log@gmail.com>
2025-02-24 20:25:24 -0300

    Ajuste no modal de cadastrar conta bancaria

--- src/app/core/models/cadastro/conta.bancaria.model.ts
+++ src/app/core/models/cadastro/conta.bancaria.model.ts
@@ -44,19 +44,6 @@ export class DadosContaBancaria {
   user_login: string;
   fator_multa: number;
   fator_juros: number;
-  envia_sms: string;
-  envia_email: string;
-  envia_whatsapp: string;
-  Aviso_Cobranca_Criada: string;
-  Aviso_Dia_Vencimento: string;
-  Aviso_Cobranca_Recebida: string;
-  Aviso_Linha_Digitavel: string;
-  Aviso_Cobranca_Vencida: string;
-  Aviso_Cobranca_Atualizada: string;
-  Aviso_Dias_Apos_Vencimento: string;
-  Dias_Apos_Vencimento: number;
-  Aviso_Dias_Antes_Vencimento: string;
-  Dias_Antes_Vencimento: number;
   Dias_Cancelar_Registro: number;
 }
 
